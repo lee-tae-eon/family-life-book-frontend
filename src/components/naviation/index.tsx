fd2b11ba3f9106f6a4a0d80a89f48db61490e6e2
이태언 <mktears0609@gmail.com>
2022-09-04 21:00:28 +0900

    ADD: navigation

--- src/components/naviation/index.tsx
+++ src/components/naviation/index.tsx
@@ -1,9 +1,10 @@
 function Navigation() {
   return (
-    <nav className="flex items-center w-full h-16">
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
+    <nav className="flex items-center justify-between w-full h-16 border-b-[1px]">
+      <div>LOGO</div>
+      <div className="">
+        <div>로그인</div>
+      </div>
     </nav>
   );
 }

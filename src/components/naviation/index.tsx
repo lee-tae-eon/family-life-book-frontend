440d7d4e13def606c9b8ff1d1e0a100a346c3e8e
이태언 <mktears0609@gmail.com>
2022-09-05 23:42:23 +0900

    ADD: footer

--- src/components/naviation/index.tsx
+++ src/components/naviation/index.tsx
@@ -1,6 +1,6 @@
 function Navigation() {
   return (
-    <nav className="flex items-center justify-between w-full h-16 border-b-[1px]">
+    <nav className="flex items-center justify-between w-full h-16 border-b-[1px] fixed top-0">
       <div>LOGO</div>
       <div className="">
         <div>로그인</div>

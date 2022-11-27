a2e16c513f3493ea6e6592a16f5c625427a12e02
이태언 <mktears0609@gmail.com>
2022-11-27 21:14:22 +0900

    TEST: foramt

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Layout from "components/layout";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const format = new Intl.NumberFormat();
   return (
     <Layout>
       <div className="text-amber-400">test</div>
